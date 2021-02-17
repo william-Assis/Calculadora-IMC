42d93eea1e59ea18ea496a2b7cfd0e8f1b83b70f
WilliamAssis <willianfaik@gmail.com>
2021-02-17 02:43:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+<nav class="container">
+                <nav class="titulo">Calculadora-IMC</nav>
+
+                <nav class="input">
+                        <label>Nome:</label>
+                        <input type="text" id="nome">
+                </nav>
+
+                <nav class="input">
+                        <label>Altura:</label>
+                        <input type="number" id="altura">
+                </nav>
+
+                <nav class="input">
+                        <label>Peso:</label>
+                        <input type="number" id="peso">
+                </nav>
+
+                    <button id="calcular">Calcular</button>
+
+                    <nav class="result" id="resultado">Resultado</nav>
+
+            </nav>
+
+            <link href="style.css" rel="stylesheet" type="text/css" />
+            <script src="script.js"></script>
